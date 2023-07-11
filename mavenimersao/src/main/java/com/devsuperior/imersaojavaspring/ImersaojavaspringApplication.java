f879543422593f01eb9c4514fa84a802b9aecaa5
clarammenezes <clarammenezes@gmail.com>
2023-07-11 00:13:06 -0300

    Projeto criado

---
+++ mavenimersao/src/main/java/com/devsuperior/imersaojavaspring/ImersaojavaspringApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.imersaojavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImersaojavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImersaojavaspringApplication.class, args);
+	}
+
+}
